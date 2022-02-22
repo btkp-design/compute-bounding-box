8189be2d89479b8077aa1507b701246f87f38c21
Heli Aldridge <heli@heli.pet>
2022-02-21 21:00:01 -0500

    sort includes and remove unneeded ones

--- bounding-onion.cpp
+++ bounding-onion.cpp
@@ -1,15 +1,8 @@
-#include <CGAL/Simple_cartesian.h>
-#include <CGAL/Polygon_2.h>
-#include <CGAL/point_generators_2.h>
-#include <CGAL/random_convex_set_2.h>
-#include <CGAL/min_quadrilateral_2.h>
-#include <CGAL/optimal_bounding_box.h>
-#include <CGAL/Exact_predicates_exact_constructions_kernel.h>
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
-#include <CGAL/Exact_predicates_exact_constructions_kernel_with_sqrt.h>
 #include <CGAL/IO/OBJ.h>
+#include <CGAL/optimal_bounding_box.h>
 #include <CGAL/Optimal_bounding_box/Oriented_bounding_box_traits_3.h>
-
+#include <CGAL/random_convex_set_2.h>
 #include <iostream>
 
 using K = CGAL::Exact_predicates_inexact_constructions_kernel;
