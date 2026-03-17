9041d22f096837ff7d32bcb796360afe2527dd0f
btkp-design <bpobiel@gmail.com>
2026-03-17 13:22:54 -0500

    Take file paths as arguments, makefile changes (#3)
    * Take file paths as arguments, remove redundant input file check, add newline
    
    * Change compiler to posix, build flags, misc changes

--- Makefile
+++ Makefile
@@ -1,28 +1,30 @@
 CXX=x86_64-w64-mingw32-c++
-CFLAGS=-O3
+CFLAGS := -O3 
+BUILD_FLAGS = -std=c++17 -pthread -DCGAL_DO_NOT_USE_BOOST_MP -DCGAL_EIGEN3_ENABLED
+
 EIGEN_DIR = /usr/include/eigen3
+CGAL_VER := 6.0.3
+CGAL_DIR := $(CURDIR)/CGAL-$(CGAL_VER)/include
+BOOST_DIR := $(CURDIR)/boost_1_83_0
+
+INCLUDES := -I$(CGAL_DIR) -I$(BOOST_DIR) -I$(EIGEN_DIR)
 
 .PHONY: all dependencies clean
 
 all: compute-bounding-box.exe
 
-compute-bounding-box: bounding-onion.cpp
-	$(CXX) $(CFLAGS) -ICGAL-6.0.3/include/ -Iboost_1_83_0/ -DCGAL_DO_NOT_USE_BOOST_MP -DCGAL_EIGEN3_ENABLED bounding-onion.cpp \
-	-I$(EIGEN_DIR) \
-	-o $@
+compute-bounding-box.exe: bounding-onion.cpp
+	$(CXX) $(CFLAGS) $(INCLUDES) $(BUILD_FLAGS) $< -o $@
 
-compute-bounding-box.exe: compute-bounding-box
-	cp $^ $@
-
-CGAL-6.0.3.tar.xz:
-	curl -OL https://github.com/CGAL/cgal/releases/download/v6.0.3/CGAL-6.0.3.tar.xz
+CGAL-$(CGAL_VER).tar.xz:
+	curl -OL https://github.com/CGAL/cgal/releases/download/v$(CGAL_VER)/CGAL-$(CGAL_VER).tar.xz
 
 boost_1_83_0.tar.bz2:
 	curl -OL https://archives.boost.io/release/1.83.0/source/boost_1_83_0.tar.bz2
 
-dependencies: CGAL-6.0.3.tar.xz boost_1_83_0.tar.bz2
-	-rm -r CGAL-6.0.3 boost_1_83_0
-	tar -xf CGAL-6.0.3.tar.xz
+dependencies: CGAL-$(CGAL_VER).tar.xz boost_1_83_0.tar.bz2
+	-rm -r CGAL-$(CGAL_VER) boost_1_83_0
+	tar -xf CGAL-$(CGAL_VER).tar.xz
 	tar -xf boost_1_83_0.tar.bz2
 
 clean:
--- bounding-onion.cpp
+++ bounding-onion.cpp
@@ -11,7 +11,6 @@
 #include <iostream>
 #include <fstream>
 #include <cstring>
-//#include "csv.h"
 #include <filesystem>
 #include <vector>
 #include <sstream>
@@ -21,10 +20,7 @@ using K = CGAL::Exact_predicates_inexact_constructions_kernel;
 typedef CGAL::Surface_mesh<K::Point_3> Surface_mesh;
 typedef K::Point_3 Point_3;
 typedef K::Plane_3 Plane_3;
-
 namespace PMP = CGAL::Polygon_mesh_processing;
-// argc = number of args passed to script,
-// argv = array of strings (char*'s) representing arguments to script
 
 std::string& ltrim(std::string& s, const char* t = " \t\n\r\f\v")
 {
@@ -32,77 +28,96 @@ std::string& ltrim(std::string& s, const char* t = " \t\n\r\f\v")
     return s;
 }
 
+void printUsage() {
+    std::cerr << "Usage: ./compute-bounding-box.exe <input.obj> <output.obj> <output.csv>" << std::endl;
+    std::cerr << "  <input.obj>   - Full path and filename of the input OBJ file" << std::endl;
+    std::cerr << "  <output.obj>  - Full path and filename for the output OBJ file" << std::endl;
+    std::cerr << "  <output.csv>  - Full path and filename for the output CSV file" << std::endl;
+}
+
 int main(int argc, char** argv)
 {
-    if (argc < 2) {
-        std::cerr << "Please use: ./compute-bounding-box.exe <input.obj> <output_filename>" << std::endl;
+    if (argc != 4) {
+        std::cerr << "Error: Wrong number of arguments." << std::endl;
+        printUsage();
         return 1;
     }
 
-    const char* input_path = argv[1]; //Filename prepends to argv[0], hence why we use argv[1]
+    const std::string input_path  = argv[1];
+    const std::string output_path_obj = argv[2];
+    const std::string output_path_csv = argv[3];
 
-    std::string output_path = "";
-    std::string output_path_obj = "";
-    std::string output_path_stl = "";
-    std::string output_path_csv = "";
-
-    if (argc == 3) {    //Check to make sure the right number of arguments were passed
-        output_path = argv[2];
-        output_path_obj = output_path + "_OBB.obj";
-        output_path_stl = output_path + "_OBB.stl";
-        output_path_csv = output_path + "_OBB.csv";
-
-        if (output_path.empty()){   //Check to make sure there's a legitimate output specified
-            std::cerr << "You didn't specify an output filename. Please use: ./compute-bounding-box.exe <input.obj> <output_filename>" << std::endl;
-            return 1;
-        }
-    } else {
-        std::cerr << "Wrong number of arguments. Please use: ./compute-bounding-box.exe <input.obj> <output_filename>" << std::endl;
+    // Validate that input path is not empty
+    if (input_path.empty()) {
+        std::cerr << "Error: Input file path cannot be empty." << std::endl;
+        printUsage();
         return 1;
     }
 
-    std::vector<K::Point_3> points_ref;     //Initialize vector of 3D points to store obj points
-    std::vector<std::vector<std::size_t> > faces_ref;   //Initialize vector to store obj faces
-
-    //Read the obj file, output error if no path
-    if (!CGAL::IO::read_OBJ(input_path, points_ref, faces_ref)) {
-        std::cerr << "error reading OBJ" << std::endl;
+    // Validate that output paths are not empty
+    if (output_path_obj.empty() || output_path_csv.empty()) {
+        std::cerr << "Error: Output file paths cannot be empty." << std::endl;
+        printUsage();
         return 1;
     }
 
-    std::array<K::Point_3, 8> obb_points;       //Create array for storing bounding box points
-    CGAL::oriented_bounding_box(points_ref, obb_points);    //create OBB
+    // Ensure output directories exist
+    auto obj_parent = std::filesystem::path(output_path_obj).parent_path();
+    auto csv_parent = std::filesystem::path(output_path_csv).parent_path();
 
-    // This just lists off all the points in the terminal
-    for(int i = 0; i < 8; i += 1) {
-        std::cout << obb_points[i] << std::endl;
+    if (!obj_parent.empty() && !std::filesystem::exists(obj_parent)) {
+        std::cerr << "Error: Output OBJ directory does not exist: " << obj_parent << std::endl;
+        return 1;
+    }
+    if (!csv_parent.empty() && !std::filesystem::exists(csv_parent)) {
+        std::cerr << "Error: Output CSV directory does not exist: " << csv_parent << std::endl;
+        return 1;
     }
 
-    // Construct OBB
+    std::vector<K::Point_3> points_ref;
+    std::vector<std::vector<std::size_t>> faces_ref;
+
+    if (!CGAL::IO::read_OBJ(input_path.c_str(), points_ref, faces_ref)) {
+        std::cerr << "Error: Could not read OBJ file: " << input_path << std::endl;
+        return 1;
+    }
+
+    std::array<K::Point_3, 8> obb_points;
+    CGAL::oriented_bounding_box(points_ref, obb_points);
+
+    // Print bounding box points to terminal
+    for (int i = 0; i < 8; i++) {
+        std::cout << "Point " << i << ": " << obb_points[i] << std::endl;
+    }
+
+    // Construct and write OBB mesh
     Surface_mesh obb_sm;
     CGAL::make_hexahedron(obb_points[0], obb_points[1], obb_points[2], obb_points[3],
-                            obb_points[4], obb_points[5], obb_points[6], obb_points[7], obb_sm);
+                          obb_points[4], obb_points[5], obb_points[6], obb_points[7], obb_sm);
     PMP::triangulate_faces(obb_sm);
-    CGAL::IO::write_OBJ(output_path_obj, obb_sm);
 
-    //Start by creating objects to store the .csv lines
-    std::ifstream fin;
-    std::string line_in;
-    std::string CSV_line;
+    if (!CGAL::IO::write_OBJ(output_path_obj, obb_sm)) {
+        std::cerr << "Error: Could not write OBJ file: " << output_path_obj << std::endl;
+        return 1;
+    }
+    std::cout << "OBJ written to: " << output_path_obj << std::endl;
 
-    // Save OBB points to file - Needed for generating Slicer fiducials
-    try
-    {
+    // Write OBB points to CSV
+    try {
         std::ofstream myFile(output_path_csv);
-        myFile << obb_points[0] << "," << obb_points[1] << "," << obb_points[2] << "," << obb_points[3] <<
-                "," << obb_points[4] << "," << obb_points[5] << "," << obb_points[6] << "," << obb_points[7];
+        if (!myFile.is_open()) {
+            std::cerr << "Error: Could not open CSV file for writing: " << output_path_csv << std::endl;
+            return 1;
+        }
+        myFile << obb_points[0] << "," << obb_points[1] << "," << obb_points[2] << "," << obb_points[3]
+               << "," << obb_points[4] << "," << obb_points[5] << "," << obb_points[6] << "," << obb_points[7];
         myFile.close();
+        std::cout << "CSV written to: " << output_path_csv << std::endl;
     }
-    catch (const std::exception &e)
-    {
-        std::cerr << "Caught " << e.what() << " while trying to save OBB .csv file" << std::endl;
+    catch (const std::exception& e) {
+        std::cerr << "Error writing CSV: " << e.what() << std::endl;
+        return 1;
     }
-
 
     return 0;
 }
